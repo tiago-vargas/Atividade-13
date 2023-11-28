01bdc17fa6c08270ec954c365ea7c53897c4a18a
Tiago Vargas <tiago.ce10@outlook.com>
2023-11-28 11:44:50 -0300

    Detect K_4 as planar

--- fun_test.py
+++ fun_test.py
@@ -11,3 +11,14 @@ class TestPlanarity:
 		graph = Graph(adjacency_matrix)
 
 		assert graph.is_planar() is True
+
+	def test_if_k4_is_planar(self):
+		adjacency_matrix = [
+			[0, 1, 1, 1],
+			[1, 0, 1, 1],
+			[1, 1, 0, 1],
+			[1, 1, 1, 0],
+		]
+		graph = Graph(adjacency_matrix)
+
+		assert graph.is_planar() is True
